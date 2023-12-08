36760057cae349d1e22698784285a12fddec5a5d
JoeMickey <87237660+JoeMickey@users.noreply.github.com>
2023-12-07 16:46:34 -0800

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,36 @@ import time_series_analysis
 
 def read_dataset():
     '''
+    Combines all of the file data and lists the amount of citations per day in order
     :return: DataFrame of our dataset
     '''
-    df = pd.read_csv('database.csv')
-    return df
+
+    read_list_rev = ['parking_citations_2012_part1_datasd.csv', 'parking_citations_2012_part2_datasd.csv',
+                'parking_citations_2013_part1_datasd.csv', 'parking_citations_2013_part2_datasd.csv',
+                'parking_citations_2014_part1_datasd.csv', 'parking_citations_2014_part2_datasd.csv',
+                'parking_citations_2015_part1_datasd.csv', 'parking_citations_2015_part2_datasd.csv',
+                'parking_citations_2016_part1_datasd.csv', 'parking_citations_2016_part2_datasd.csv',
+                'parking_citations_2017_part1_datasd.csv', 'parking_citations_2017_part2_datasd.csv',
+                'parking_citations_2018_part1_datasd.csv', 'parking_citations_2018_part2_datasd.csv',
+                ]
+    read_list_norm = ['parking_citations_2019_part1_datasd.csv', 'parking_citations_2019_part2_datasd.csv',
+                'parking_citations_2020_part1_datasd.csv', 'parking_citations_2020_part2_datasd.csv',
+                'parking_citations_2021_part1_datasd.csv', 'parking_citations_2021_part2_datasd.csv',
+                'parking_citations_2022_part1_datasd.csv', 'parking_citations_2022_part2_datasd.csv',
+                'parking_citations_2023_part1_datasd.csv', 'parking_citations_2023_part2_datasd.csv'
+                ]
+    
+    con_list = []
+    for f in read_list_rev:
+        df = pd.read_csv('./Datasets/Parking Ticket Datasets' + f) # make sure to change this to the correct python read path
+        df = df.reindex(index=df.index[::-1])
+        con_list.append(df)
+    for f in read_list_norm:
+        df = pd.read_csv('./Datasets/Parking Ticket Datasets' + f) # make sure to change this to the correct python read path
+        con_list.append(df)
+    
+    output_df = pd.concat(con_list)
+    return output_df
 
 
 if __name__ == "__main__":
